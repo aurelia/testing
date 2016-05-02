cd9bd78a0905c0e4900a6f4fbc67ef1e30990075
Martin Gustafsson <gusten86@gmail.com>
2016-05-02 22:22:38 +0200

    feat(component-tester): support lifecycle testing

--- src/component-tester.js
+++ src/component-tester.js
@@ -1,4 +1,5 @@
 import {bootstrap} from 'aurelia-bootstrapper';
+import {View} from 'aurelia-templating';
 
 export const StageComponent = {
   withResources(resources): ComponentTester {
@@ -7,28 +8,39 @@ export const StageComponent = {
 };
 
 export class ComponentTester {
+  bind: {(bindingContext: any): void };
+  attached: {(): void };
+  unbind: {(): void };
+  dispose: {(): Promise<any> };
+  element: Element;
+  viewModel: any;
   _html: string;
   _resources: string | string[] = [];
   _bindingContext: any;
+  _rootView: View;
   _configure = aurelia => aurelia.use.standardConfiguration();
-  element;
 
   bootstrap(configure: (aurelia: Aurelia) => void) {
     this._configure = configure;
   }
 
-  withResources(resources): ComponentTester {
+  withResources(resources: string | string[]): ComponentTester {
     this._resources = resources;
     return this;
   }
 
-  inView(html): ComponentTester {
+  inView(html: string): ComponentTester {
     this._html = html;
     return this;
   }
 
-  boundTo(bindingContext): ComponentTester {
+  boundTo(bindingContext: any): ComponentTester {
     this._bindingContext = bindingContext;
+    return this;
+  }
+
+  manuallyHandleLifecycle(): ComponentTester {
+    this._prepareLifecycle();
     return this;
   }
 
@@ -36,17 +48,53 @@ export class ComponentTester {
     return bootstrap(aurelia => {
       return Promise.resolve(this._configure(aurelia)).then(() => {
         aurelia.use.globalResources(this._resources);
-
         return aurelia.start().then(a => {
           let host = document.createElement('div');
           host.innerHTML = this._html;
-
           document.body.appendChild(host);
           aurelia.enhance(this._bindingContext, host);
-
+          this._rootView = aurelia.root;
           this.element = host.firstElementChild;
+          this.viewModel = this.element.au.controller.viewModel;
+          this.dispose = () => host.parentNode.removeChild(host);
+          return new Promise(resolve => setTimeout(() => resolve(), 0));
         });
       });
+    });
+  }
+
+  _prepareLifecycle() {
+    // bind
+    const bindPrototype = View.prototype.bind;
+    View.prototype.bind = () => {};
+    this.bind = bindingContext => new Promise(resolve => {
+      View.prototype.bind = bindPrototype;
+      if (bindingContext !== undefined) {
+        this._bindingContext = bindingContext;
+      }
+      this._rootView.bind(this._bindingContext);
+      setTimeout(() => resolve(), 0);
+    });
+
+    // attached
+    const attachedPrototype = View.prototype.attached;
+    View.prototype.attached = () => {};
+    this.attached = () => new Promise(resolve => {
+      View.prototype.attached = attachedPrototype;
+      this._rootView.attached();
+      setTimeout(() => resolve(), 0);
+    });
+
+    // detached
+    this.detached = () => new Promise(resolve => {
+      this._rootView.detached();
+      setTimeout(() => resolve(), 0);
+    });
+
+    // unbind
+    this.unbind = () => new Promise(resolve => {
+      this._rootView.unbind();
+      setTimeout(() => resolve(), 0);
     });
   }
 }
