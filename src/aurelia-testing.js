d1a0533f5a462f5df8f711798aab7a9b1ec95bdd
Rob Eisenberg <rob@bluespire.com>
2016-05-31 11:35:14 -0400

    fix(index): correct export strategy

--- src/aurelia-testing.js
+++ src/aurelia-testing.js
@@ -1,6 +1,15 @@
-export function configure(config) {
+import {CompileSpy} from './compile-spy';
+import {ViewSpy} from './view-spy';
+
+function configure(config) {
   config.globalResources(
     './compile-spy',
     './view-spy'
   );
 }
+
+export {
+  CompileSpy,
+  ViewSpy,
+  configure
+};
