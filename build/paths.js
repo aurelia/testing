26039dbe641f1dc9c09612412f627f4dde310219
Rob Eisenberg <rob@bluespire.com>
2016-06-15 07:36:38 -0400

    chore(all): update build, deps, ci, d.ts gen, doc gen, lint

--- build/paths.js
+++ build/paths.js
@@ -1,10 +1,14 @@
 var path = require('path');
 var fs = require('fs');
 
+// hide warning //
+var emitter = require('events');
+emitter.defaultMaxListeners = 20;
+
 var appRoot = 'src/';
 var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-module.exports = {
+var paths = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
@@ -13,5 +17,15 @@ module.exports = {
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/',
-  packageName: pkg.name
+  packageName: pkg.name,
+  useTypeScriptForDTS: false,
+  importsToAdd: [],
+  sort: true
 };
+
+paths.ignore = ['aurelia-testing.js'];
+paths.files = [
+  paths.source
+];
+
+module.exports = paths;
