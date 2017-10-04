8ed88ad59c71b75fe78f2019d5986b86bbda2c07
Michael de Wit <michael@drillster.com>
2017-10-04 11:47:57 +0200

    chore(everything): refactor to TypeScript build
    - Use TS build from aurelia/validation
    - Refactor project to TypeScript, including linter fixes
    - Fix incorrect typings for `waitFor`, `waitForElement`, and `waitForElements` (#72)
    - Add missing typing for `ComponentTester.detached`
    - Fix incorrect return types for `bind`, `attached`, and `unbind` (return value is a `Promise`)
    - Make `ComponentTester` generic, so a contained view-model type can be passed in
    - Fix return type of `dispose`
    - Change `bindingContext` type from any to `{}`, as it has to be an `Object`
    - Add Karma middleware to serve templates
    - Update README.md to reflect build changes
    
    Closes #72, possible breaking changes for TypeScript users

---
+++ src/compile-spy.ts
@@ -0,0 +1,22 @@
+import { customAttribute, TargetInstruction } from 'aurelia-templating';
+import { inject } from 'aurelia-dependency-injection';
+import { getLogger } from 'aurelia-logging';
+import { DOM } from 'aurelia-pal';
+
+/**
+ * Attribute to be placed on any element to have it emit the View Compiler's
+ * TargetInstruction into the debug console, giving you insight into all the
+ * parsed bindings, behaviors and event handers for the targeted element.
+ */
+@customAttribute('compile-spy')
+@inject(DOM.Element, TargetInstruction)
+export class CompileSpy {
+  /**
+   * Creates and instanse of CompileSpy.
+   * @param element target element on where attribute is placed on.
+   * @param instruction instructions for how the target element should be enhanced.
+   */
+  constructor(element: Element, instruction: TargetInstruction) {
+    getLogger('compile-spy').info(element.toString(), instruction);
+  }
+}
