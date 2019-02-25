671153f43a6def0f55701db00ecf8e9caa779316
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-02-26 00:46:59 +1100

    chore(tests): add more tests for ignore deps

--- test/setup.ts
+++ test/setup.ts
@@ -12,5 +12,7 @@ PLATFORM.moduleName('test/resources/my-component');
 PLATFORM.moduleName('test/resources/my-attribute');
 PLATFORM.moduleName('test/resources/composes/compose-1');
 PLATFORM.moduleName('test/resources/composes/compose-2');
+PLATFORM.moduleName('test/resources/no-deps/el1');
+PLATFORM.moduleName('test/resources/no-deps/el2');
 PLATFORM.moduleName('test/resources/html-only/html-only1.html');
 PLATFORM.moduleName('test/resources/html-only/html-only2.html');
--- test/template-dependency-stub.spec.ts
+++ test/template-dependency-stub.spec.ts
@@ -4,6 +4,84 @@ import { bootstrap } from 'aurelia-bootstrapper';
 
 describe('Template dependency stubbing', () => {
 
+  it('ignores deps per ComponentTesterInstance', async () => {
+    const component1 = await StageComponent
+      .withResources(['test/resources/no-deps/el1', 'test/resources/no-deps/el2'])
+      .inView('<el1></el1><el2><el1></el1></el2>')
+      .ignoreDependencies('test/resources/no-deps/el2');
+
+    const component2 = await StageComponent
+      .withResources(['test/resources/no-deps/el1', 'test/resources/no-deps/el2'])
+      .inView('<el1></el1><el2><el1></el1></el2>')
+      .ignoreDependencies('test/resources/no-deps/el1');
+
+    await Promise.all([
+      component1.create(bootstrap),
+      component2.create(bootstrap)
+    ]);
+
+    // component1 works
+    expect(component1['host'].textContent).toEqual('el1el1', 'component1.host.textContent');
+    component1['host'].querySelectorAll('.au-target').forEach(el => {
+      el.removeAttribute('class');
+      el.removeAttribute('au-target-id');
+    });
+    expect(component1['host'].innerHTML).toEqual(
+      '<el1>el1</el1><el2><el1>el1</el1></el2>',
+      'component1.host.innerHTML'
+    );
+    component1.dispose();
+
+    // component2 works
+    expect(component2['host'].textContent).toEqual('el2', 'component2.textContent');
+    component2['host'].querySelectorAll('.au-target').forEach(el => {
+      el.removeAttribute('class');
+      el.removeAttribute('au-target-id');
+    });
+    expect(component2['host'].innerHTML).toEqual(
+      '<el1></el1><el2>el2</el2>',
+      'component2.host.innerHTML'
+    );
+
+    component2.dispose();
+  });
+
+  describe('with global resources', () => {
+    it('discards globally declared resources', async () => {
+      const component = await StageComponent
+        .withResources(['test/resources/no-deps/el1', 'test/resources/no-deps/el2'])
+        .inView('<el1></el1><el2><el1></el1></el2>')
+        .ignoreDependencies('test/resources/no-deps/el2');
+      await component.create(bootstrap);
+
+      expect(component['host'].textContent).toEqual('el1el1', 'host.textContent');
+      component['host'].querySelectorAll('.au-target').forEach(el => {
+        el.removeAttribute('class');
+        el.removeAttribute('au-target-id');
+      });
+      expect(component['host'].innerHTML).toEqual(
+        '<el1>el1</el1><el2><el1>el1</el1></el2>',
+        'host.innerHTML'
+      );
+      component.dispose();
+    });
+
+    it('works with all deps discarded', async () => {
+      const component = await StageComponent
+        .withResources(['test/resources/no-deps/el1', 'test/resources/no-deps/el2'])
+        .inView('<el1></el1><el2><el1></el1></el2>')
+        .ignoreDependencies('test/resources/no-deps/el1', 'test/resources/no-deps/el2');
+      await component.create(bootstrap);
+
+      expect(component['host'].textContent).toEqual('', 'host.textContent');
+      expect(component['host'].innerHTML).toEqual(
+        '<el1></el1><el2><el1></el1></el2>',
+        'host.innerHTML'
+      );
+      component.dispose();
+    });
+  });
+
   describe('with normal custom element', () => {
 
     it('prevents the matching dependency from being loaded even if a template <require>s it', async () => {
